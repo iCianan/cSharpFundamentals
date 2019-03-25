bdf4503e049666a8ccee8ee874c4b6b9122fe3dc
Keenan Irvin Benning <Keenan.IrvinBenning@twomen.com>
2019-03-25 09:12:39 -0400

    init commit

---
+++ cSharpFundementals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharpFundementals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
