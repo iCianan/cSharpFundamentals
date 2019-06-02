353f9dd6c143def6ab7fdf3db39f74d98d0600fc
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-06-02 19:15:51 -0400

    init commit for ps c# fund course

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GradeBook
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 2)
+			{
+				Console.WriteLine($"Hello {args[0]} {args[1]}!");
+			} 
+			else
+			{
+				Console.WriteLine("Welcome!");
+			}				
+		}
+	}
+}
