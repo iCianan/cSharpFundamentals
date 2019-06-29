565143ca58ef7d401fd354d659009cfd50eead4a
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-06-28 21:35:47 -0400

    Added LongestCommonPrefix and IsParenthesesValid leetcode questions

---
+++ cSharpFundementals/LeetCode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpFundementals
+{
+  class LeetCode
+  {
+    public string LongestCommonPrefix(string[] strs)
+    {
+      if (strs.Length == 0) return "";
+      string pre = strs[0];
+      for (int i = 1; i < strs.Length; i++)
+        while (strs[i].IndexOf(pre) != 0)
+          pre = pre.Substring(0, pre.Length - 1);
+      return pre;
+    }
+    public bool IsParenthesesValid(string s)
+    {
+      //() help from the future --- use a stack
+      var checker = new Dictionary<char, char>();
+      checker.Add('(', ')');
+      checker.Add('[', ']');
+      checker.Add('{', '}');
+
+      //var checker1 = (title:"Lost", author: "Sam");
+      bool results = false;
+      for (int i = 0; i < s.Length; i++)
+      {
+        if (checker.ContainsKey(s[i]))
+        {
+          if (s.IndexOf(checker[s[i]]) == 0)
+          {
+            results = false;
+          }
+          else
+          {
+            results = true;
+          }
+        }
+      }
+      return results;
+
+    }
+  }
+}
