ff2645f53f3704e42cff713c6cec32bfce7925b5
Keenan Irvin Benning <Keenan.IrvinBenning@twomen.com>
2019-09-03 16:51:33 -0400

    Solving stats challenge

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -6,14 +6,16 @@ namespace GradeBook
 {
     public class Book
     {
-        private List<double> grades { get; set; }
-        public Book()
+        private List<double> _grades { get; set; }
+        private string _name { get; set; }
+        public Book(string name)
         {
-            grades = new List<double>();
+            _grades = new List<double>();
+            _name = name;
         }
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            _grades.Add(grade);
         }
         public void ShowStatistics()
         {
@@ -21,13 +23,13 @@ namespace GradeBook
             var lowGrade = double.MaxValue;
             var results = 0.0;
 
-            foreach (var number in grades)
+            foreach (var number in _grades)
             {
                 lowGrade = Math.Min(number, lowGrade);
                 highGrade = Math.Max(number, highGrade);
                 results += number;
             }
-            results /= grades.Count;
+            results /= _grades.Count;
             Console.WriteLine($"The lowest grade is {lowGrade}");
             Console.WriteLine($"The highest grade is {highGrade}");
             Console.WriteLine($"The average grade is {results:N1}");
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,8 +6,8 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
-            var book = new Book();
+        {           
+            var book = new Book("KB's Grade Book");
             var numbers = new[] { 3.32, .21, 5.45, 13.9, 393.7621 };
             List<double> grades = new List<double>() { 32.324, 43.432, 9.2313, 0.9310, 20.1 };
             foreach (var number in grades)
