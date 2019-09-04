a788f845fdda1a50ba7dcae79e0337dfe20f9ce6
Keenan Irvin Benning <Keenan.IrvinBenning@twomen.com>
2019-09-04 09:57:21 -0400

    Adding generics to program

---
+++ cSharpFundementals/Generics.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace cSharpFundementals
+{
+    public class Generics
+    {
+        public static string[] GenerateNamesArray()
+        {
+            string[] names = new string[4];
+            names[0] = "Gamma";
+            names[1] = "Vlissides";
+            names[2] = "Johnson";
+            names[3] = "Helm";
+            return names;
+        }
+        public static ArrayList GenerateNamesArrayList()
+        {
+            ArrayList names = new ArrayList();
+            names.Add("Gamma");
+            names.Add("Vlissides");
+            names.Add("Johnson");
+            names.Add("Helm");
+            return names;
+        }
+        public static StringCollection GenerateNameStringCollection()
+        {
+            StringCollection names = new StringCollection();
+            names.Add("Gamma");
+            names.Add("Vlissides");
+            names.Add("Johnson");
+            names.Add("Helm");
+            return names;
+        }
+        public static List<string> GenerateNames()
+        {
+            List<string> names = new List<string>();
+            names.Add("Gamma");
+            names.Add("Vlissides");
+            names.Add("Johnson");
+            names.Add("Helm");
+            return names;
+        }
+        public static void PrintNames(List<string> names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
--- cSharpFundementals/Program.cs
+++ cSharpFundementals/Program.cs
@@ -2,35 +2,39 @@
 
 namespace cSharpFundementals
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      //   Operators op = new Operators("JC","Penny", 54);
-      // Console.WriteLine(op.FirstName);
-      //  Console.WriteLine(op.A);
+        static void Main(string[] args)
+        {
+            //   Operators op = new Operators("JC","Penny", 54);
+            // Console.WriteLine(op.FirstName);
+            //  Console.WriteLine(op.A);
 
-      //LinkedList ll = new LinkedList();
-      //ll.insert(5);
-      //Console.WriteLine(ll.Head);
-      //ll.insert(15);
-      //ll.insert(24);
-      //ll.insert(75);
-      //ll.insert(14);
-      //ll.insert(37);
+            LinkedList ll = new LinkedList();
+            ll.insert(5);
+            //Console.WriteLine(ll.Head);
+            //ll.insert(15);
+            //ll.insert(24);
+            //ll.insert(75);
+            //ll.insert(14);
+            //ll.insert(37);
 
-      //TreesAndGraphs.BinarySearchTree bst = new TreesAndGraphs.BinarySearchTree();
-      //bst.Insert(5);
+            //TreesAndGraphs.BinarySearchTree bst = new TreesAndGraphs.BinarySearchTree();
+            //bst.Insert(5);
 
 
-      //FizzBuzz fizz = new FizzBuzz();
-      //int[] nums = { 2, 5, 12, 15, 20, 24, 30 }; // buzz fizzbuzz buzz fizz fizzbuzz
-      //fizz.fizzBuzz(nums);
-      string[] stringArray = { "tom", "t", "tomato", "toffee", "tommy" };
-      LeetCode leetCode = new LeetCode();
-      leetCode.LongestCommonPrefix(stringArray);
-      leetCode.IsParenthesesValid("[]()}");
+            ////FizzBuzz fizz = new FizzBuzz();
+            ////int[] nums = { 2, 5, 12, 15, 20, 24, 30 }; // buzz fizzbuzz buzz fizz fizzbuzz
+            ////fizz.fizzBuzz(nums);
+            //string[] stringArray = { "tom", "t", "tomato", "toffee", "tommy" };
+            //LeetCode leetCode = new LeetCode();
+            //leetCode.LongestCommonPrefix(stringArray);
+            //leetCode.IsParenthesesValid("[]()}");
+            var names = Generics.GenerateNamesArrayList();
+            Generics.PrintNames(names);
+            
 
+
+        }
     }
-  }
 }
