6894b6105c4061348c8cfdd44cf3b7b311239d45
Keenan Irvin Benning <Keenan.IrvinBenning@twomen.com>
2019-09-09 21:34:36 -0400

    first crack at accepting user input

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,17 +6,23 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
             var book = new Book("KB's Grade Book");
-            var numbers = new[] { 3.32, .21, 5.45, 13.9, 393.7621 };
-            List<double> grades = new List<double>() { 32.324, 43.432, 9.2313, 0.9310, 20.1 };
-            foreach (var number in grades)
-            {   
-                book.AddGrade(number);
-            }
-            foreach (var number in numbers)
+            var log = new Logger();
+            log.Log("Welcome to GradeBook Platinum. Press Q when you are done");
+            log.Log("Enter your name: ");
+            var input = Console.ReadLine();
+            book.ChangeName(input);
+
+            while (input != "q")
             {
-                book.AddGrade(number);
+                log.Log("Enter your grade: ");
+                input = Console.ReadLine();
+                if (input?.ToLower() == "q" || input == "")
+                {
+                    break;
+                }
+                book.AddGrade(Convert.ToDouble(input));
             }
             var stats = book.ComputeStatistics();
             book.ShowStatistics(stats);
