4e5ea0d1ab9150f1af2b8de7cc101a3e20254b45
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-09-11 20:30:57 -0400

    added test for letter grade

--- gradebook/test/GradeBook.Tests/BookTests.cs
+++ gradebook/test/GradeBook.Tests/BookTests.cs
@@ -3,34 +3,34 @@ using System;
 
 namespace GradeBook.Tests
 {
-    public class BookTests
+  public class BookTests
+  {
+    [Test]
+    public void ShouldComputeAccurateStatistics()
     {
-        [Test]
-        public void ShouldComputeAccurateStatistics()
-        {
-            //arrange
-            var book = new Book("");
-            book.AddGrade(72.3);
-            book.AddGrade(91.3);
-            book.AddGrade(97.7);
-            //act
-            var results = book.ComputeStatistics();
+      //arrange
+      var book = new Book("");
+      book.AddGrade(72.3);
+      book.AddGrade(91.3);
+      book.AddGrade(97.7);
+      //act
+      var results = book.ComputeStatistics();
 
 
-            //assert
-            Assert.That(results.High, Is.EqualTo(97.7));
-            Assert.That(results.Low, Is.EqualTo(72.3d));
-            Assert.That(results.Average, Is.EqualTo(87.1).Within(0.1));
-
-        }
-
-        [Test]
-        public void ShouldNotAcceptLargeNumbers()
-        {
-            var book = new Book("");
-            var ex = Assert.Throws<ArgumentException>(
-                () => book.AddGrade(105));
-            Assert.That(ex.Message, Is.EqualTo("Invalid grade"));
-        }
+      //assert
+      Assert.That(results.High, Is.EqualTo(97.7));
+      Assert.That(results.Low, Is.EqualTo(72.3d));
+      Assert.That(results.Average, Is.EqualTo(87.1).Within(0.1));
+      Assert.That(results.letter, Is.EqualTo('B'));
     }
+
+    [Test]
+    public void ShouldNotAcceptLargeNumbers()
+    {
+      var book = new Book("");
+      var ex = Assert.Throws<ArgumentException>(
+          () => book.AddGrade(105));
+      Assert.That(ex.Message, Is.EqualTo("Invalid grade"));
+    }
+  }
 }
