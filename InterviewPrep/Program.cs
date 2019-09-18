76f74437c90c19517535ff216be315a74c608fdf
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-09-17 22:28:46 -0400

    binary search and linear search

--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -6,11 +6,13 @@ namespace InterviewPrep
     {
         static void Main(string[] args)
         {
-            Arrays arrays = new Arrays();
-            int[] numbers = { 15, 8, 62, 1, -1 };
-            int targetSum = 2;
-            arrays.TwoNumberSum(numbers, targetSum);
-            arrays.BubbleSort(numbers);
+            int[] nums = new int[]{ 2, 4, 6, 8, 11, 23, 44, 56, 65, 78, 99 };
+            Search search = new Search();
+            bool results = search.IsUserNameTaken("john");
+           int yep = search.BinarySearch(nums, 78);
+           int nope = search.BinarySearch(nums, 8);
+            Console.WriteLine(yep);
+            Console.WriteLine(nope);
         }
     }
 }
---
+++ InterviewPrep/Search.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep
+{
+    public class Search
+    {
+        public string[] usernames = new[] { "john", "jeff" };
+        public string[] states = new[]
+        {
+            "Alaska",
+            "Alabama",
+            "Arkansas",
+            "American Samoa",
+            "Arizona",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "District of Columbia",
+            "Delaware",
+            "Florida",
+            "Georgia",
+            "Guam",
+            "Hawaii",
+            "Iowa",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Massachusetts",
+            "Maryland",
+            "Maine",
+            "Michigan",
+            "Minnesota",
+            "Missouri",
+            "Mississippi",
+            "Montana",
+            "North Carolina",
+            "North Dakota",
+            "Nebraska",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "Nevada",
+            "New York",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Pennsylvania",
+            "Puerto Rico",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Virginia",
+            "Virgin Islands",
+            "Vermont",
+            "Washington",
+            "Wisconsin",
+            "West Virginia",
+            "Wyoming"
+        };
+
+        public bool IsUserNameTaken(string username)
+        {
+            return usernames.Contains<string>(username);
+        }
+        public bool IsStateValid(string username)
+        {
+            
+            return states.Contains<string>(username);
+        }
+        public int BinarySearch(int[] array, int value)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            int mid = ((right + left) / 2);
+
+            while (left <= right)
+            {
+                if (array[mid] == value)
+                {
+                    return mid;
+                }
+                else if (array[mid] > value)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+
+                }
+                mid = ((right + left) / 2);
+            }
+            return -1;
+        }
+    }
+}
