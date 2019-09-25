6b8c7bb83b9d5313ed2a582793a883bc43c9b058
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-09-25 19:42:56 -0400

    adding queues

--- Core/Program.cs
+++ Core/Program.cs
@@ -6,9 +6,23 @@ namespace Core
     {
         static void Main(string[] args)
         {
-            Arrays arrays = new Arrays();
-            arrays.ListDaysInOrder();
-            arrays.ReplaceAllDaysWithMonths();
+            //Arrays arrays = new Arrays();
+            //arrays.ListDaysInOrder();
+            //arrays.ReplaceAllDaysWithMonths();
+            Queue queue = new Queue();
+            int[] numbers =
+            {
+                1,5,7,11,15,17,24,26,28,32,36,39,40,45,52,53,58,59,63,64,69,72,77,78,98,99
+            };
+            foreach (var num in numbers)
+            {
+                queue.Enqueue(num);
+            }
+            foreach (var num in numbers)
+            {
+                queue.Dequeue();
+            }
+
         }
     }
 }
---
+++ Core/Queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Queue
+    {
+        public Node First { get; private set; }
+        public Node Last { get; private set; }
+        public int Size { get; private set; }
+        public Queue()
+        {
+            First = null;
+            Last = null;
+            Size = 0;
+        }
+
+        public int Enqueue(int val)
+        {
+            Node newNode = new Node(val);
+            if (First == null)
+            {
+                First = newNode;
+                Last = newNode;
+                Size++;
+                return Size;
+            }            
+            Last.Next = newNode;
+            Last = newNode;
+            Size++;
+            return Size;
+            
+        }
+
+        public Node Dequeue()
+        {
+            if (First == null)
+            {
+                throw new Exception("Nothing in queue");
+            }
+            Node current = First;
+            First = current.Next;
+            if (First == null)
+            {
+                Last = First;
+                Size--;
+                return current;
+                
+            }
+            Size--;
+            return current;
+        }
+
+        public class Node
+        {
+            public int Val { get; private set; }
+            public Node Next { get; set; }
+            public Node(int val)
+            {
+                Val = val;
+                Next = null;
+            }
+        }
+    }
+}
