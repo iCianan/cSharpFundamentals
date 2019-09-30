840f5459fe4515aed807b7d2df3c2fd471e8df35
Keenan Irvin-Benning <kirvinb@gmail.com>
2019-09-29 20:48:57 -0400

    update to 3.0 and graph class

---
+++ Core/Graph.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Graph
+    {
+
+        public Dictionary<string, List<string>> adjacencyList { get; set; }
+
+        public Graph()
+        {
+            adjacencyList = new Dictionary<string, List<string>>();
+        }
+
+        public void AddVertex(string key)
+        {
+            if (!adjacencyList.ContainsKey(key))
+            {
+                adjacencyList[key] = new List<string>();
+            }
+        }
+        public void AddEdge(string v1, string v2)
+        {
+            if (adjacencyList.ContainsKey(v1) && adjacencyList.ContainsKey(v2))
+            {
+                adjacencyList[v1].Add(v2);
+                adjacencyList[v2].Add(v1);
+            }
+
+        }
+        public void RemoveEdge(string v1, string v2)
+        {
+            if (adjacencyList.ContainsKey(v1) && adjacencyList.ContainsKey(v2))
+            {
+                adjacencyList[v1].Remove(v2);
+                adjacencyList[v2].Remove(v1);
+            }
+        }
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -8,20 +8,99 @@ namespace Core
         {
             //Arrays arrays = new Arrays();
             //arrays.ListDaysInOrder();
-            //arrays.ReplaceAllDaysWithMonths();
-            Queue queue = new Queue();
-            int[] numbers =
-            {
-                1,5,7,11,15,17,24,26,28,32,36,39,40,45,52,53,58,59,63,64,69,72,77,78,98,99
+            ////arrays.ReplaceAllDaysWithMonths();
+            //Queue queue = new Queue();
+            //int[] numbers =
+            //{
+            //    1,5,7,11,15,17,24,26,28,32,36,39,40,45,52,53,58,59,63,64,69,72,77,78,98,99
+            //};.
+            string[] states = {
+    "Alaska",
+    "Alabama",
+    "Arkansas",
+    "American Samoa",
+    "Arizona",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "District of Columbia",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Iowa",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Massachusetts",
+    "Maryland",
+    "Maine",
+    "Michigan",
+    "Minnesota",
+    "Missouri",
+    "Mississippi",
+    "Montana",
+    "North Carolina",
+    "North Dakota",
+    "Nebraska",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "Nevada",
+    "New York",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Virginia",
+    "Virgin Islands",
+    "Vermont",
+    "Washington",
+    "Wisconsin",
+    "West Virginia",
+    "Wyoming"
             };
-            foreach (var num in numbers)
+            //foreach (var num in numbers)
+            //{
+            //    queue.Enqueue(num);
+            //}
+            //foreach (var num in numbers)
+            //{
+            //    Console.WriteLine(queue.Dequeue());                
+            //}
+            Graph graph = new Graph();
+
+            foreach (string state in states)
             {
-                queue.Enqueue(num);
+                graph.AddVertex(state);
             }
-            foreach (var num in numbers)
-            {
-                Console.WriteLine(queue.Dequeue());                
-            }
+
+            graph.AddEdge("Alaska", "California");
+            graph.AddEdge("California", "Kansas");
+            graph.AddEdge("Kansas", "Michigan");
+            graph.AddEdge("Kansas", "Texas");
+            graph.AddEdge("Texas", "Florida");
+            graph.AddEdge("Michigan", "California");
+            graph.AddEdge("Michigan", "Florida");
+            graph.AddEdge("Michigan", "Pennsylvania");
+            graph.AddEdge("Pennsylvania", "North Carolina");
+            graph.AddEdge("North Carolina", "Florida");
+
+            graph.RemoveEdge("Michigan", "Florida");
+            Console.WriteLine(graph);
+
+
         }
     }
 }
