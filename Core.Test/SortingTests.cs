889e7c6bd6b436c2e0dbc2fc86a16c5edb82ca5f
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-04-28 22:28:37 -0400

    Add quick sort, but dont pretend like you understand it. You gotta alot of work to do

--- Core.Test/SortingTests.cs
+++ Core.Test/SortingTests.cs
@@ -11,7 +11,7 @@ namespace Core.Test
         [SetUp]
         public void Setup()
         {
-            unsortedArray = new int[] { 21, 11, 8, 3, -11, 6, 14, 9 };
+            unsortedArray = new int[] { 21, 11, -11, 8, 3, 6, 14, 9 };
             sorted = new int[] { -11, 3, 6, 8, 9, 11, 14, 21 };
         }
 
@@ -49,6 +49,27 @@ namespace Core.Test
         public void MergeSort_Should_ReturnSortedArray_When_GivenNonEmptyArray()
         {
             int[] actual = MergeSort.Sort(unsortedArray);
+
+            Assert.That(actual, Is.EqualTo(sorted));
+        }
+
+        [Test]
+        [Category("QuickSort")]
+        public void QuickSort_Should_ReturnEmptyArray_When_InputArrayIsEmpty()
+        {
+            int[] expected = { };
+            QuickSort.Sort(expected, 0, expected.Length);
+
+            Assert.That(expected, Is.Empty);
+        }
+
+        [Test]
+        [Category("QuickSort")]
+        public void QuickSort_Should_ReturnSortedArray_When_GivenNonEmptyArray()
+        {
+            QuickSort.Sort(unsortedArray, 0, unsortedArray.Length -1);
+            int[] actual = unsortedArray;
+            
 
             Assert.That(actual, Is.EqualTo(sorted));
         }
---
+++ Core/QuickSort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class QuickSort
+    {
+        public static int[] Sort(int[] numbers)
+        {
+            Sort(numbers, 0, numbers.Length - 1);
+            return numbers;
+        }
+        public static void Sort(int[] numbers, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int pivot = start;
+            int left = start + 1;
+            int right = end;
+
+            while (right >= left)
+            {
+                if (numbers[left] > numbers[pivot] && numbers[right] < numbers[pivot])
+                {
+                    swap(numbers, left, right);
+                }
+
+                if (numbers[left] <= numbers[pivot])
+                {
+                    left++;
+                }
+                if (numbers[right] >= numbers[pivot])
+                {
+                    right--;
+                }                
+            }
+            swap(numbers, pivot, right);
+            bool leftSubarrayIsSmaller = right - 1 - start < end - (right + 1);
+            if (leftSubarrayIsSmaller)
+            {
+                Sort(numbers, start, right - 1);
+                Sort(numbers, right + 1, end);
+            } else
+            {
+                Sort(numbers, right + 1, end);
+                Sort(numbers, start, right -1);
+            }
+        }
+
+        private static int Partition(int[] numbers, int start, int end)
+        {
+            int pivot = numbers[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end; j++)
+            {
+                if (numbers[j] <= pivot)
+                {
+                    i++;
+                    swap(numbers, i, j);
+
+                }
+            }
+            swap(numbers, i + 1, end);
+            return i + 1;
+        }
+
+        private static void swap(int[] numbers, int left, int right)
+        {
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+
+        }
+    }
+}
