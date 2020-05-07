e63feb7f73329119cc0b5a94a7ab13c78392aad2
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-05-06 22:24:30 -0400

    added question to source code

--- Questions/BalancedBrackets.cs
+++ Questions/BalancedBrackets.cs
@@ -37,10 +37,18 @@ namespace Questions
             {
                 return false;
             }
-            return true;   
+            return true;
         }
 
-        // Not working 
+        /*
+        Write a function that takes in a string made up of brackets ([{ }]) and other optional characters.
+        The function should return a boolean representing whether the string is balanced with regards to brackets.
+
+        A string is said to be balanced if it has as many opening brackets of a certain type as it has closing 
+        brackets of that type and if no bracket is unmatched.Note that an opening bracket can't match a corresponding 
+        closing bracket that comes before it, and similarly, a closing bracket can't match a corresponding opening 
+        bracket that comes after it.Also, brackets can't overlap each other as in [(])
+        */
 
         public bool BunchOfBrackets(string expression)
         {
