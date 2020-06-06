50ea7cb34f08829771a97dccec52bc99559bf272
Keenan Irvin-Benning <keenan@catalystdigitalventures.com>
2020-06-06 14:23:05 -0400

    a bit of BST work

---
+++ Core.Test/BSTTests.cs
@@ -0,0 +1,92 @@
+﻿
+using NUnit.Framework;
+
+namespace Core.Test
+{
+    public class BSTTests
+    {
+        private int[] sut;
+        [SetUp]
+        public void Setup()
+        {
+            sut = new int[] { -11, 3, 6, 8, 11, 14, 21 };
+        }
+
+        [Test]
+        public void Insert_Should_AddNewNodeToLeftChild_When_ParentIsLarger()
+        {
+            //arrange
+            var BST = new BST(7);
+
+            //act
+            var result = BST.Insert(3);
+
+            //assert
+            Assert.That(BST.left, Is.EqualTo(result));
+        }
+        [Test]
+        public void Insert_Should_AddNewNodeToRightChild_When_ParentIsSmaller()
+        {
+            //arrange
+            var BST = new BST(3);
+
+            //act
+            var result = BST.Insert(7);
+
+            //assert
+            Assert.That(BST.right, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Insert_Should_AddNewNodeToTree_When_GivenAnInteger()
+        {
+            //arrange
+            var BST = new BST(3);
+
+            //act
+            foreach (var number in sut)
+            {
+                BST.Insert(number);
+            }
+
+            //assert
+            Assert.That(BST.right.right.value, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Contains_Should_ReturnTrue_When_TreeContainsRequestedValue()
+        {
+            //arrange
+            var BST = new BST(3);
+
+            //act
+            foreach (var number in sut)
+            {
+                BST.Insert(number);
+            }
+
+            var results = BST.Contains(21);
+
+            //assert
+            Assert.That(results, Is.True);
+        }
+
+        [Test]
+        public void Contains_Should_ReturnFalse_When_TreeDoesntContainsRequestedValue()
+        {
+            //arrange
+            var BST = new BST(3);
+
+            //act
+            foreach (var number in sut)
+            {
+                BST.Insert(number);
+            }
+
+            var results = BST.Contains(57);
+
+            //assert
+            Assert.That(results, Is.False);
+        }
+    }
+}
---
+++ Core/BST.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class BST
+    {
+        public BST left;
+        public BST right;
+        public int value;
+
+        public BST(int value)
+        {
+            this.value = value;
+        }
+
+        public BST Insert(int value)
+        {
+            BST newNode = new BST(value);
+            BST current = this;
+            while (true)
+            {
+                if (current.value > value)
+                {
+                    if (current.left == null)
+                    {
+                        current.left = newNode;
+                        return newNode;
+                    }
+                    else
+                    {
+                        current = current.left;
+                    }
+                }
+                else
+                {
+                    if (current.right == null)
+                    {
+                        current.right = newNode;
+                        return newNode;
+                    }
+                    else
+                    {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+        public bool Contains(int value)
+        {
+            if (this.value == value) return true;
+            var current = this;
+            while (true)
+            {
+                if (current.value > value)
+                {
+                    if (current.left == null) return false;
+                    else
+                    {
+                        current = current.left;
+                    }
+                }
+                else if (current.value < value)
+                {
+                    if (current.right == null) return false;
+                    else
+                    {
+                        current = current.right;
+                    }
+                }
+                else return true;
+            }
+        }
+    }
+}
