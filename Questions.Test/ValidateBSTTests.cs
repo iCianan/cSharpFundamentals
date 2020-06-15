23797b9c98e8270a27131815d4cabcc2eb9565a7
Keenan Irvin-Benning <keenan@catalystdigitalventures.com>
2020-06-15 08:30:47 -0400

    bsts

---
+++ Questions.Test/ValidateBSTTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using NUnit.Framework;
+
+namespace Questions.Test
+{
+    [TestFixture]
+    public class ValidateBSTTests
+    {
+        [Test]
+        public void Insert_Should_AddNewNodeToTree_When_GivenAnInteger()
+        {
+            //arrange
+            var BST = new BST(10);
+            var sut = new[] { 15,22,13,14,5,5,2,1};
+            foreach (var number in sut)
+            {
+                BST.Insert(number);
+            }
+
+            //act
+            bool results = ValidateBST.ValidateInOrder(BST);
+
+            //assert
+            Assert.That(results, Is.True);
+        }
+    }
+}
---
+++ Questions/ValidateBST.cs
@@ -0,0 +1,46 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    public static class ValidateBST
+    {
+        public static bool Validate(BST root)
+        {
+            if (root == null) return false;
+            var current = root;
+            while (true)
+            {
+                if (current.left.value < current.right.value)
+                {
+                    current = current.left;
+                }
+                else if (current.right.value > current.left.value)
+                {
+                    current = current.left;
+                }
+                else return false;
+            }            
+        }
+        public static bool ValidateInOrder(BST root)
+        {
+            if (root == null) return false;
+            if (root.right == null & root.left == null) return true;
+            var isLeftValid = ValidateInOrder(root.left);
+            var isRightValid = root.right == null && ValidateInOrder(root.right) == false ?  true: ValidateInOrder(root.right);
+            var rootVal = root.value;
+            if (isLeftValid == true && isRightValid == true && rootVal > root.left.value && rootVal <= root.right.value)
+            {
+                return true;
+            }
+            return false;
+            
+            
+
+        }
+    }
+}
