7bb58d57aba2a52979dd6bdc7a0d2cde0c63ed3e
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-06-16 23:12:01 -0400

    same question using frerquency counters

---
+++ Questions.Test/SameTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions.Test
+{
+    [TestFixture]
+    public class SameTests
+    {
+        [Test]
+        public void Same_ReturnsTrue_When_SecondArrayContainsSquaredValuesOfFirstArray()
+        {
+            var sample = new[] { 1, 2, 3 };
+            var sampleSquared = new[] { 4, 1, 9 };
+
+            var results = Same.isSame(sample, sampleSquared);
+
+            Assert.That(results, Is.True);
+        }
+
+        [Test]
+        public void Same_ReturnsTrue_When_SecondArrayContainsSquaredValuesOfFirstArrayRegardlessOfOrder()
+        {
+            var sample = new[] { 1, 2, 3 };
+            var sampleSquared = new[] { 1, 9, 4 };
+
+            var results = Same.isSame(sample, sampleSquared);
+
+            Assert.That(results, Is.True);
+        }
+
+        [Test]
+        public void Same_ReturnsFalse_When_SecondArrayContainsSquaredValuesOfFirstArrayButIncorrectFrequency()
+        {
+            var sample = new[] { 1, 2, 1 };
+            var sampleSquared = new[] { 4, 4 , 1};
+
+            var results = Same.isSame(sample, sampleSquared);
+
+            Assert.That(results, Is.False);
+        }
+    }
+}
---
+++ Questions/Same.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    public class Same
+    {
+        // Write a function called same, which accepts two arrays.
+        // The function should return true if every value in the array has its 
+        // corresponding value squared in the second array. The frequency of values must 
+        // the same. 
+        // O(n) T | O(n) S
+        public static bool isSame(int[] first, int[] second)
+        {
+            var counter = new Dictionary<int, int>();
+            var counter2 = new Dictionary<int, int>();
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (counter.ContainsKey(first[i]))
+                {
+                    counter[first[i]] = counter[first[i]] += 1;
+                }
+                else counter.Add(first[i], 1);
+            }
+            for (int i = 0; i < second.Length; i++)
+            {
+                if (counter2.ContainsKey(second[i]))
+                {
+                    counter2[second[i]] = counter2[second[i]] += 1;
+                }
+                else counter2.Add(second[i], 1);
+                    
+            }
+
+            foreach (var item in counter)
+            {
+                if (!counter2.ContainsKey(item.Key* item.Key))
+                {
+                    return false;
+                }
+
+                if (counter2[item.Key * item.Key] != counter[item.Key])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
