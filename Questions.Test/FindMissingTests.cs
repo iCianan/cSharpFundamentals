b678feadda92f2f82b7718135b2a5cdd33fee5bf
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-06-17 22:41:16 -0400

    find missing

---
+++ Questions.Test/FindMissingTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions.Test
+{
+    [TestFixture]
+    public class FindMissingTests
+    {
+        [Test]
+        public void FindMissing_ReturnMissingNumber_WhenGivenTwoIdenticalArraysWithOneMissingOneNumber()
+        {
+            int[] first = new int[] { 4, 5, 5, 9 };
+            int[] second = new int[] {  5, 5 ,9 };
+
+            var results = FindMissing.Find(first, second);
+
+            Assert.That(results, Is.EqualTo(4));
+        }
+    }
+}
---
+++ Questions/FindMissing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    public static class FindMissing
+    {
+        public static int Find(int[] first, int[] second)
+        {
+            int result = int.MaxValue;
+            var firstCache = new Dictionary<int, int>();
+            var secondCache = new Dictionary<int, int>();
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (firstCache.ContainsKey(first[i]))
+                {
+                    firstCache[first[i]] += 1;
+                }
+                else firstCache[first[i]] = 1;
+            }
+
+            for (int i = 0; i < second.Length; i++)
+            {
+                if (secondCache.ContainsKey(second[i]))
+                {
+                    secondCache[second[i]] += 1;
+                }
+                else secondCache[second[i]] = 1;
+            }
+
+            foreach (var item in first)
+            {
+                if (!secondCache.ContainsKey(item) || secondCache[item] != firstCache[item])
+                {
+                    result = item;
+                }
+            }
+            return result;
+        }
+    }
+}
