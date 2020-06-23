d5b9bb80ed0215e18eac423850d98a139c15a773
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-06-23 11:15:11 -0400

    group anagrams

---
+++ Questions.Test/GroupAnagramsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions.Test
+{
+    [TestFixture]
+    public class GroupAnagramsTests
+    {
+        [Test]
+        public void GroupAnagrams_Returns_ListOfGroupAnagrams()
+        {
+            var anagrams = new List<string>() { "yo", "act", "flop", "tac", "cat", "oy", "olfp" };
+
+            var results = GroupAnagrams.find(anagrams);
+
+            Assert.That(results.Count, Is.EqualTo(3));
+            
+        }
+            
+
+    }
+}
---
+++ Questions/GroupAnagrams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    public static class GroupAnagrams
+    {
+        public static List<List<string>> find(List<string> strings)
+        {
+            var anagrams = new Dictionary<string, List<string>>();
+            foreach (string anagram in strings)
+            {
+                var charArray = anagram.ToCharArray();
+                Array.Sort(charArray);
+                var sortedWord = new string(charArray);
+                if (anagrams.ContainsKey(sortedWord))
+                {
+                    anagrams[sortedWord].Add(anagram);
+                }
+                else
+                {
+                    anagrams[sortedWord] = new List<string>() { anagram };
+                }
+            }
+            return anagrams.Values.ToList();
+        }
+    }
+}
